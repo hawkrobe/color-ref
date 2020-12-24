be2e1776d7f02e07ccc84e375b20752a67f29bb1
Sonia Murthy <sonia@Sonias-MacBook-Pro.local>
2020-12-24 12:45:37 -0800

    compare expectation ratings to ground truth

--- analyses/expectation_analysis.py
+++ analyses/expectation_analysis.py
@@ -16,53 +16,84 @@ import matplotlib
 # INITIALIZE DATA
 #---------------------------------------------------------------------------------
 
-# LOAD ALL COLOR PICKER DATA
-df = pd.read_csv(r'../data/norming/expectationData-all.csv')
+# LOAD ALL EXPECTATION RATING DATA
+df_expectation = pd.read_csv(r'../data/norming/expectationData-all.csv')
 # select columns for word and RGB color response
 cols = ["word", "sliderResponse"]
-df = df[cols]
+df_expectation = df_expectation[cols]
 
-# CHANGE CONDITION TO SELECT DIFFERENT BLOCK
-# df = df[df['condition'] == 'block1_target_trial']
-block = "both"
+# LOAD COLOR PICKER DATA FOR BLOCK 2 (EXPECTATION RESPONSES)
+df_block2 = pd.read_csv(r'../data/norming/colorPickerData-all.csv')
+# select columns for word, button response, and condition (which block)
+cols = ["word", "button_pressed", "condition"]
+df_block2 = df_block2[cols]
+# select all block2_target_trial rows
+df_block2 = df_block2[df_block2['condition'] == 'block2_target_trial']
 
-# LOAD ORDERED LIST OF WORDS BY ENTROPY
-df_entropy = pd.read_csv(r'./entropy/sorted-entropies-all-%s.csv' % block)
-df_entropy = df_entropy.rename(columns={block: "entropy"})
+# LOAD ORDERED LIST OF WORDS BY ENTROPY for BLOCK 2 RESPONSES
+df_entropy = pd.read_csv(r'./entropy/sorted-entropies-all-block2.csv')
 words = df_entropy['word'].to_list()
 
-# LOAD VARIANCES
-df_var = pd.read_csv(r'./variance/sorted-variances-all-%s.csv' % block)
-df_var = df_var.rename(columns={block: "variacne"})
+#---------------------------------------------------------------------------------
+# CREATE COLUMN FOR CONDITION
+
+expectationCondition = []
+concrete = words[:100]
+abstract = words[-100:]
+
+for index, row in df_expectation.iterrows():
+    if row['word'] in concrete:
+        expectationCondition.append('concrete')
+
+    else:
+        expectationCondition.append('abstract')
+
+# add condition column to df
+df_expectation['condition'] = expectationCondition
+print(df_expectation)
+df_expectation.to_csv("./expectation/expectations-by-condition.csv", index=False)
 
 #---------------------------------------------------------------------------------
 # HELPER FUNCTIONS
 #---------------------------------------------------------------------------------
 
-# function to select all RGB points that belong to a particular target words
-def selectPoints(df, word):
+# function to select all data of type type in df for word  that belong to a particular target word
+def selectData(df, word, type):
     points = df[df['word'] == word]
-    return points['sliderResponse'].to_list()
-
+    return points[type].to_list()
 
 #---------------------------------------------------------------------------------
-# COMPUTE AVG EXPECTATION
+# COMPUTE GROUND TRUTH
 #---------------------------------------------------------------------------------
+dfWords = []
+groundTruth = []
+groundTruthCondition = []
+# get percentages of response for each possible choice
+for word_index, word in enumerate(words):
+    numChoices = 88
+    responses = selectData(df_block2, word, 'button_pressed')
 
-ratings = []
-condition = []
-concrete = words[:100]
-abstract = words[-100:]
+    # for each button response, count number of responses for that particular button color
+    # divide it by total number of responses to get percentage of bar
+    percentage = []
+    for i in range(numChoices):
+        numResponses = responses.count(i)
+        p = float(numResponses)/float(len(responses))
+        percentage.append(p)
 
-for index, row in df.iterrows():
-    if row['word'] in concrete:
-        condition.append('concrete')
+    # Loop through every block 2 response and see what percent of others
+    # shared their response (i.e. append the percentage at index=this button response)
+    # multiply value by 100 to get percent of others
+    for response_index, response in enumerate(responses):
+        dfWords.append(word)
+        groundTruth.append(round(percentage[response]*100))
+        if word in concrete:
+            groundTruthCondition.append('concrete')
+        else:
+            groundTruthCondition.append('abstract')
 
-    else:
-        condition.append('abstract')
-
-# add condition column to df
-df['condition'] = condition
-print(df)
-
-df.to_csv("./expectation/expectations-by-condition.csv", index=False)
+# save to dataframe
+df_groundTruth = pd.DataFrame({'word':dfWords, 'groundTruth':groundTruth, 'condition':groundTruthCondition}, columns=['word', 'groundTruth', 'condition'])
+print(df_groundTruth)
+# save df as csv
+df_groundTruth.to_csv("./expectation/ground-truth-by-condition.csv", index=False)
